7a8e2bcc89f41d393b8c9d5c85de856e58d5ce33
Michael <michaelleojacob@gmail.com>
2021-11-04 14:45:34 -0700

    wrote my first tests following the get started instructions from the jest website. Got a few cases working. Need to write the readme, and finish the current page i'm following.

---
+++ sum.test.js
@@ -0,0 +1,36 @@
+const sum = require('./sum');
+
+test('adds 1 + 2 to equal 3', () => {
+  expect(sum(1, 2)).toBe(3);
+});
+
+test('two plus two is four', () => {
+  expect(2 + 2).toBe(4);
+});
+
+// In this code, expect(2 + 2) returns an "expectation" object.
+// You typically won't do much with these expectation
+// objects except call matchers on them.
+// In this code, .toBe(4) is the matcher.
+// When Jest runs, it tracks all the failing matchers
+// so that it can print out nice error messages for you.
+
+// toBe uses Object.is to test exact equality.
+// If you want to check the value of an object, use toEqual instead:
+test('object assignment', () => {
+  const data = { one: 1 };
+  data['two'] = 2;
+  expect(data).toEqual({ one: 1, two: 2 });
+});
+
+//toEqual recursively checks every field of an object or array.
+
+//You can also test for the opposite of a matcher:
+
+test('adding positive numbers is not zero', () => {
+  for (let a = 1; a < 10; a++) {
+    for (let b = 1; b < 10; b++) {
+      expect(a + b).not.toBe(0);
+    }
+  }
+});
